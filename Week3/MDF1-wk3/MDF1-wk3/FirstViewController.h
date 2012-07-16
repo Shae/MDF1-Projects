cc50acbedd4226213916b15dd67233df8a8cedc2
Shae <shae@fullsail.com>
2012-07-16 14:20:45 -0700

    Setting up Week 3 Project File

---
+++ Week3/MDF1-wk3/MDF1-wk3/FirstViewController.h
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.h
+//  MDF1-wk3
+//
+//  Created by Shae Klusman on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+{
+    NSString *businessName;
+    NSString *longitude;
+    NSNumber *latitude;
+    
+}
+@end
---
+++ Week3/MDF1-wk3/MDF1-wk3/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MDF1-wk3
+//
+//  Created by Shae Klusman on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
