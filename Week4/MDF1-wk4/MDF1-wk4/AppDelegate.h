d1bddad2715e5098d631f578556410e2e208117b
Shae <shae@fullsail.com>
2012-07-24 16:40:30 -0700

    Week4 setup

---
+++ Week4/MDF1-wk4/MDF1-wk4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDF1-wk4
+//
+//  Created by Shae Klusman on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UISplitViewController *splitViewController;
+
+@end
---
+++ Week4/MDF1-wk4/MDF1-wk4/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  MDF1-wk4
+//
+//  Created by Shae Klusman on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
