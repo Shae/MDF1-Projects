fd731419fa6060e12fe54cd02b3655cc00410653
Shae <shae@fullsail.com>
2012-07-24 17:31:31 -0700

    delete first setup

---
+++ Week4/MDF1-wk4/MDF1-wk4/TabTwoViewControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabTwoViewControllerViewController.h
+//  MDF1-wk4
+//
+//  Created by Shae Klusman on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabTwoViewControllerViewController : UIViewController
+
+@end
---
+++ Week4/MDF1-wk4/MDF1-wk4/TabTwoViewControllerViewController.m
@@ -0,0 +1,44 @@
+//
+//  TabTwoViewControllerViewController.m
+//  MDF1-wk4
+//
+//  Created by Shae Klusman on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TabTwoViewControllerViewController.h"
+
+@interface TabTwoViewControllerViewController ()
+
+@end
+
+@implementation TabTwoViewControllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
